3e3fdab144c9532e58aa605c91917a5fc7cc5d59
Matthew <mrideout1985@gmail.com>
2021-10-04 23:37:29 +0100

    mobile stytling

--- components/footer.js
+++ components/footer.js
@@ -1,30 +1,29 @@
 import React from 'react'
 import Link from "next/link"
 import { Github, Linkedin, Gmail } from "../components/icons/index"
+import { useMedia } from 'react-use';
+
 import styles from "./footer.module.scss"
 
 const Footer = () => {
+	const isWide = useMedia('(max-width: 500px)');
 	return (
 		<footer className={styles.footer}>
 			<div className={styles.name}>
 				Matthew Rideout
 			</div>
 			<div className={styles.contact}>
-
 				<a href="https://github.com/mrideout1985">
-					<Github size={48} />
+					<Github size={isWide ? 25 : 46} />
 				</a>
 
 				<a href="https://www.linkedin.com/in/matthew-rideout-6b184a19b/">
-					<Linkedin size={48} />
+					<Linkedin size={isWide ? 25 : 46} />
 				</a>
 
 				<a href="mailto:mrideout.dev@gmail.com, mrideout1985@gmail.com">
-					<Gmail size={48} />
+					<Gmail size={isWide ? 25 : 46} />
 				</a>
-
-
-
 			</div>
 		</footer>
 	)
