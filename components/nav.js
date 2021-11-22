97b8ffc04de38ddc9a1e91aaeeb40682f09861cb
mrideout1985 <mrideout1985@gmail.com>
2021-11-22 11:14:14 +0000

    moved toggle bar to stop useEffect triggering

--- components/nav.js
+++ components/nav.js
@@ -1,52 +1,70 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import styles from "./nav.module.scss";
 import { useRouter } from "next/router";
+import { DarkToggle } from "../components/toggle/DarkToggle";
+import { useMediaQuery } from "react-responsive";
 
 const Nav = () => {
-    const router = useRouter();
+  const router = useRouter();
+  const DARK_CLASS = "dark";
+  const systemPrefersDark = useMediaQuery(
+    {
+      query: "(prefers-color-scheme: dark)",
+    },
+    undefined,
+    (prefersDark) => {
+      setIsDark(prefersDark);
+    }
+  );
 
-    return (
-        <header className={styles.header}>
-            <nav className={styles.nav}>
-                <div className={styles.logo}>
-                    <Link href="/">
-                        <a href="">MR</a>
-                    </Link>
-                </div>
-                <div className={styles.links}>
-                    <Link href="/">
-                        <a
-                            className={[
-                                [styles["default"]],
-                                [
-                                    router.pathname === "/"
-                                        ? styles["active"]
-                                        : "",
-                                ],
-                            ].join(" ")}
-                        >
-                            Home
-                        </a>
-                    </Link>
-                    <Link href="/projects">
-                        <a
-                            className={[
-                                [styles["default"]],
-                                [
-                                    router.pathname === "/projects"
-                                        ? styles["active"]
-                                        : "",
-                                ],
-                            ].join(" ")}
-                        >
-                            Projects
-                        </a>
-                    </Link>
-                </div>
-            </nav>
-        </header>
-    );
+  const [isDark, setIsDark] = useState(systemPrefersDark);
+
+  useEffect(() => {
+    if (isDark) {
+      document.documentElement.classList.add(DARK_CLASS);
+    } else {
+      document.documentElement.classList.remove(DARK_CLASS);
+    }
+  }, [isDark]);
+
+  return (
+    <header className={styles.header}>
+      <nav className={styles.nav}>
+        <div className={styles.logo}>
+          <Link href="/">
+            <a href="">MR</a>
+          </Link>
+        </div>
+        <div className={styles.links}>
+          <Link href="/">
+            <a
+              className={[
+                [styles["default"]],
+                [router.pathname === "/" ? styles["active"] : ""],
+              ].join(" ")}
+            >
+              Home
+            </a>
+          </Link>
+          <Link href="/projects">
+            <a
+              className={[
+                [styles["default"]],
+                [router.pathname === "/projects" ? styles["active"] : ""],
+              ].join(" ")}
+            >
+              Projects
+            </a>
+          </Link>
+        </div>
+        <DarkToggle
+          isDark={isDark}
+          onChange={(e) => setIsDark(e.target.checked)}
+        />
+      </nav>
+    </header>
+  );
 };
 
 export default Nav;
