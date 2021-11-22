7d8942227fc7a92bc618be40b1549fb187ab30aa
mrideout1985 <mrideout1985@gmail.com>
2021-11-22 11:14:31 +0000

    styling

--- components/toggle/DarkToggle.js
+++ components/toggle/DarkToggle.js
@@ -1,17 +1,17 @@
 import React from "react";
 import Toggle from "react-toggle";
 import styles from "./darktoggle.module.scss";
-
 import "react-toggle/style.css";
+import { Moon, Sun } from "../icons";
 
 export const DarkToggle = ({ isDark, onChange }) => {
-    return (
-        <Toggle
-            className={styles.toggle}
-            icons={{ checked: "🌙", unchecked: "🔆" }}
-            aria-label="Dark mode"
-            checked={isDark}
-            onChange={onChange}
-        />
-    );
+  return (
+    <Toggle
+      className={styles.toggle}
+      icons={{ checked: <Moon />, unchecked: <Sun /> }}
+      aria-label="Dark mode"
+      checked={isDark}
+      onChange={onChange}
+    />
+  );
 };
